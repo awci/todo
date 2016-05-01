33e8d51c28f178a4c706adac9aac87f1b0e8c005
Mustafa YAKUT <mustafayakut@mustafayakut.com>
2016-05-02 00:30:08 +0300

    config update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	"database": "mongodb://<username>:<password>@ds011382.mlab.com:11382/awcitodo",
+	"database": "mongodb://root:DQwx713@ds011382.mlab.com:11382/awcitodo",
 	"port": process.env.PORT || 3000,
 	"secretKey": "MustafaYAKUT"
 }
